4fc518684b332394e81bec0276c86ff06bac6398
Josh Wootonn <joshuawootonn@gmail.com>
2023-11-24 12:51:41 -0500

    refactor: remove unused ssr from history page

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -1,28 +1,10 @@
 import Head from 'next/head'
 
-import { createServerSideHelpers } from '@trpc/react-query/server'
-import { AppRouter, appRouter } from '~/server/api/root'
-import superjson from 'superjson'
-import { db } from '~/server/db'
 import { useRouter } from 'next/router'
 import { Navigation } from '~/components/navigation'
 
-export const DEFAULT_PASSAGE_REFERENCE = 'psalm 23'
-
-export async function getStaticProps() {
-    const helpers = createServerSideHelpers<AppRouter>({
-        router: appRouter,
-        ctx: { session: null, db },
-        transformer: superjson,
-    })
-
-    await helpers.passage.passage.prefetch(DEFAULT_PASSAGE_REFERENCE)
-
-    return { props: { trpcState: helpers.dehydrate() } }
-}
-
-export default function Home(props: { passage?: string }) {
-    const { push, back, asPath } = useRouter()
+export default function Home() {
+    const { asPath } = useRouter()
     const finalSlashIndex = asPath.lastIndexOf('/')
     const previousPath = asPath.slice(0, finalSlashIndex)
     console.log('previousPath', previousPath, asPath)
