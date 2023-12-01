c479e0060acd0170e3dbf30e506256523d192c55
Josh Wootonn <joshuawootonn@gmail.com>
2023-12-01 06:38:51 -0500

    feat: useRect `scrollEnd` to `scroll`

--- src/lib/hooks/useRect.ts
+++ src/lib/hooks/useRect.ts
@@ -1,19 +1,15 @@
 import { RefObject, useEffect, useState } from 'react'
+import debounce from 'debounce'
 
 export function useRect(ref: RefObject<HTMLElement>) {
     const [rect, setRect] = useState<DOMRect | null>(null)
 
     useEffect(() => {
-        window.addEventListener('resize', updateRect)
-        window.addEventListener('scrollend', updateRect)
-        updateRect()
-        return () => {
-            window.removeEventListener('scrollend', updateRect)
-            window.removeEventListener('resize', updateRect)
-        }
+        const debouncedUpdateRect = debounce(updateRect, 100)
 
         function updateRect() {
             const nextRect = ref.current?.getBoundingClientRect() ?? null
+            console.log('hit')
             if (
                 nextRect &&
                 (nextRect.top !== rect?.top ||
@@ -23,6 +19,14 @@ export function useRect(ref: RefObject<HTMLElement>) {
             ) {
                 setRect(nextRect)
             }
+        }
+
+        window.addEventListener('resize', debouncedUpdateRect)
+        window.addEventListener('scroll', debouncedUpdateRect)
+        debouncedUpdateRect()
+        return () => {
+            window.removeEventListener('scroll', debouncedUpdateRect)
+            window.removeEventListener('resize', debouncedUpdateRect)
         }
     }, [])
 
