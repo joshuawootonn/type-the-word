b6a8c3128481271667355880021bcb201d0666c9
Joshua Wootonn <joshuawootonn@gmail.com>
2023-12-18 08:13:31 -0500

    feat: update the `/privacy-policy` for google authorization

--- src/pages/privacy-policy.tsx
+++ src/pages/privacy-policy.tsx
@@ -27,15 +27,40 @@ export default function Home() {
                     <h1>Privacy Policy</h1>
                     <hr className="mx-0 w-full border-t-2  border-black" />
 
-                    <h2>Agreement</h2>
+                    <h2>What data does Type the Word collect?</h2>
                     <p>
-                        What data do I collect? Type the Word collects the
-                        following data:
+                        Type the Word collects the following data:
                         <ul>
                             <li>Email</li>
                             <li>First and last name</li>
-                            <li>Passages you have typed.</li>
+                            <li>Passages you have typed</li>
                         </ul>
+                    </p>
+                    <h2>How will Type the Word use your data?</h2>
+                    <p>
+                        Type the Word collects your data so that it can:
+                        <ul>
+                            <li>
+                                Track your progress typing through the Bible
+                            </li>
+                            <li>
+                                Send occasional update emails - no more than 6 a
+                                year
+                            </li>
+                        </ul>
+                    </p>
+                    <h2>How does Type the Word store your data?</h2>
+                    <p>
+                        Type the Word securely stores your data in a planetscale
+                        database.
+                    </p>
+                    <h2>Does Type the Word share data</h2>
+                    <p>
+                        I (Josh Wootonn) have no intention of ever sharing or
+                        selling the data collected. If the usage of Type the
+                        Word starts to outpace the free tiers of the products I
+                        am using, then my monetization strategy will be donation
+                        based.
                     </p>
 
                     <h2>Contact</h2>
