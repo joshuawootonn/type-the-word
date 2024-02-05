7c11374d67cc568a2fb029f7fed224b3e4d184e0
Joshua Wootonn <joshuawootonn@gmail.com>
2024-02-05 08:11:14 -0500

    feat: add open telemetry with axiom collector

--- next.config.mjs
+++ next.config.mjs
@@ -5,12 +5,9 @@ import { withAxiom } from 'next-axiom'
 /** @type {import("next").NextConfig} */
 const config = {
     reactStrictMode: true,
-
-    /**
-     * If you are using `appDir` then you must comment the below `i18n` config out.
-     *
-     * @see https://github.com/vercel/next.js/issues/41980
-     */
+    experimental: {
+        instrumentationHook: true
+    },
     i18n: {
         locales: ['en'],
         defaultLocale: 'en',
