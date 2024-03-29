d9b7bd6912c23e773de21041a5f0ae988ee0f0aa
Joshua Wootonn <joshuawootonn@gmail.com>
2024-03-29 07:07:27 -0400

    feat: lower the timeout again
    Just did this initially to test out the connection pool

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -4,6 +4,6 @@ import { env } from '~/env.mjs'
 import * as schema from './schema'
 import postgres from 'postgres'
 
-const client = postgres(env.POSTGRES_DATABASE_URL, { idle_timeout: 60 })
+const client = postgres(env.POSTGRES_DATABASE_URL, { idle_timeout: 1 })
 
 export const db = drizzle(client, { schema })
