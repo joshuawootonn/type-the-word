a599716e591da62ec84aeb555fd6733478d7de98
Joshua Wootonn <joshuawootonn@gmail.com>
2024-06-25 06:33:48 -0400

    feat: move why to app router

---
+++ src/app/why/layout.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from 'react'
+
+export default function PassageLayout({ children }: { children: ReactNode }) {
+    return (
+        <main className="prose relative mx-auto w-full flex-grow pt-4 text-lg dark:prose-invert lg:pt-8">
+            {children}
+        </main>
+    )
+}
