8f772193763b7e8afb35b0d189cdf3e7f88506cf
Joshua Wootonn <joshuawootonn@gmail.com>
2024-06-25 07:40:57 -0400

    feat: add fathom analytics back for app router

---
+++ src/app/fathom.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import { load, trackPageview } from 'fathom-client'
+import { useEffect, Suspense } from 'react'
+import { usePathname, useSearchParams } from 'next/navigation'
+import { env } from '~/env.mjs'
+
+function TrackPageView() {
+    const pathname = usePathname()
+    const searchParams = useSearchParams()
+
+    // Load the Fathom script on mount
+    useEffect(() => {
+        load(env.NEXT_PUBLIC_FATHOM_ID, {
+            auto: false,
+
+            // includedDomains: ['www.typetheword.site', 'typetheword.site'],
+        })
+    }, [])
+
+    // Record a pageview when route changes
+    useEffect(() => {
+        if (!pathname) return
+
+        trackPageview({
+            url: pathname + searchParams?.toString(),
+            referrer: document.referrer,
+        })
+    }, [pathname, searchParams])
+
+    return null
+}
+
+export default function Fathom() {
+    return (
+        <Suspense fallback={null}>
+            <TrackPageView />
+        </Suspense>
+    )
+}
