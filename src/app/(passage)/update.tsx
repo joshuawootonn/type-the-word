0911cc894fdecb339fc30b4db1ae39949baa3d04
Joshua Wootonn <joshuawootonn@gmail.com>
2024-06-27 08:20:08 -0400

    fix: fix quote error in update component

--- src/app/(passage)/update.tsx
+++ src/app/(passage)/update.tsx
@@ -10,7 +10,7 @@ export function Update({ className }: { className?: string }) {
             Hey there! I just did a major overhaul of how type the word works
             behind the scenes. These changes went into affect {duration} ago.
             Please email me if you find any issues. I just fixed one issue for
-            books starting with a number like "2 Thessalonians." Exciting things
+            books starting with a number like &quot;2 Thessalonians.&quot; Exciting things
             are coming!
         </div>
     )
