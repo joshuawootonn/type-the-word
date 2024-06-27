892342c68543a2ffed451aedbb12197127a4abc7
Joshua Wootonn <joshuawootonn@gmail.com>
2024-06-27 06:40:31 -0400

    fix: update server-only-api.ts to dynamically choose url

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -6,7 +6,7 @@ import { ChapterHistory } from '~/app/api/chapter-history/[passage]/route'
 
 export type Body<T> = { data: T }
 
-function getBaseUrl() {
+export function getBaseUrl() {
     if (typeof window !== 'undefined') return ''
     if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`
     return `http://localhost:${process.env.PORT ?? 3000}` //
