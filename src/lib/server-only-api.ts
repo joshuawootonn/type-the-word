2ffa2f21e5b19f4f939e8d8b9ca5c3baaba31fa2
Joshua Wootonn <joshuawootonn@gmail.com>
2024-06-27 06:40:31 -0400

    fix: update server-only-api.ts to dynamically choose url

--- src/lib/server-only-api.ts
+++ src/lib/server-only-api.ts
@@ -1,14 +1,13 @@
 import { cookies } from 'next/headers'
 import { TypingSessionLog } from '~/app/api/history/log'
 import { BookOverview } from '~/app/api/history/overview'
-
-export type Body<T> = { data: T }
+import { Body, getBaseUrl } from './api'
 
 export async function fetchHistory(): Promise<{
     log: TypingSessionLog[]
     overview: BookOverview[]
 }> {
-    const response = await fetch(`http://localhost:3000/api/history`, {
+    const response = await fetch(`${getBaseUrl()}/api/history`, {
         headers: { Cookie: cookies().toString() },
     })
 
