331f6ed3cd93d14fe1052e6a482380637a8d89b8
Joshua Wootonn <joshuawootonn@gmail.com>
2024-06-30 09:38:13 -0400

    fix: hydration error in update component

--- src/app/update.tsx
+++ src/app/update.tsx
@@ -1,14 +1,10 @@
 'use client'
 
-import { formatDistance } from 'date-fns'
-
 export function Update({ className }: { className?: string }) {
-    const duration = formatDistance(new Date(2024, 5, 30, 8, 0, 0), new Date())
-
     return (
         <div className={className}>
             Hey there! I just did a major overhaul of how type the word works
-            behind the scenes. These changes went into affect {duration} ago.
+            behind the scenes. These changes went into affect this morning.
             Please email me if you find any issues. I just fixed one issue for
             books starting with a number like &quot;2 Thessalonians.&quot;
             Exciting things are coming!
