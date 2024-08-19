b5bf3c609d4d996f8f67d3481edd77d0deceff93
Joshua Wootonn <joshuawootonn@gmail.com>
2024-08-19 08:41:17 -0400

    feat: make the footer more mobile friendly

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -26,108 +26,121 @@ export function Footer() {
         !hasSeenChangelog || (hasSeenChangelog && !hasSeenEveryChangelog)
 
     return (
-        <footer className="flex w-full items-center justify-between space-x-3 py-2 text-sm dark:text-white">
-            <a
-                className="svg-outline relative no-underline"
-                href="https://www.esv.org/"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                (ESV)
-            </a>
-            <div>/</div>
-            <DropdownMenu.Root>
-                <DropdownMenu.Trigger asChild>
-                    <button
-                        className="svg-outline relative no-underline"
-                        aria-label="Customise options"
-                    >
-                        legal
-                    </button>
-                </DropdownMenu.Trigger>
-
-                <DropdownMenu.Content
-                    className="flex flex-col border-2 border-black bg-white dark:border-white dark:bg-black"
-                    sideOffset={4}
-                    align="center"
-                    side="top"
+        <footer className="flex w-full flex-col items-center justify-between py-2 text-sm dark:text-white sm:flex-row">
+            <div className="mb-3 flex items-center justify-between space-x-3 sm:mb-0">
+                <a
+                    className="svg-outline relative no-underline"
+                    href="https://www.esv.org/"
+                    target="_blank"
+                    rel="noopener noreferrer"
                 >
-                    <DropdownMenu.Item asChild={true}>
-                        <Link
-                            className="cursor-pointer px-2 py-1 no-underline outline-none focus:bg-black focus:text-white dark:focus:bg-white dark:focus:text-black"
-                            href={'/copyright'}
+                    (ESV)
+                </a>
+                <div>/</div>
+                <DropdownMenu.Root>
+                    <DropdownMenu.Trigger asChild>
+                        <button
+                            className="svg-outline relative no-underline"
+                            aria-label="Customise options"
                         >
-                            copyright
-                        </Link>
-                    </DropdownMenu.Item>
-                    <DropdownMenu.Item asChild={true}>
-                        <Link
-                            className="cursor-pointer px-2 py-1 no-underline outline-none focus:bg-black focus:text-white dark:focus:bg-white dark:focus:text-black"
-                            href={'/privacy-policy'}
-                        >
-                            privacy
-                        </Link>
-                    </DropdownMenu.Item>
-                    <DropdownMenu.Item asChild={true}>
-                        <Link
-                            className="cursor-pointer px-2 py-1 no-underline outline-none focus:bg-black focus:text-white dark:focus:bg-white dark:focus:text-black"
-                            href={'/terms-of-service'}
-                        >
-                            terms
-                        </Link>
-                    </DropdownMenu.Item>
-                </DropdownMenu.Content>
-            </DropdownMenu.Root>
+                            legal
+                        </button>
+                    </DropdownMenu.Trigger>
 
-            <div>/</div>
-            <Link className="svg-outline relative no-underline" href={'/why'}>
-                why?
-            </Link>
-
-            <div className="hidden flex-grow md:block"> </div>
-            <div className="block md:hidden">/</div>
-            <Link
-                className="svg-outline relative no-underline"
-                href={'https://github.com/joshuawootonn/type-the-word'}
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                github
-            </Link>
-
-            <div>/</div>
-            <Link className={clsx(`svg-outline relative`)} href={'/changelog'}>
-                changelog
-                {hasUnreadChangelog && (
-                    <svg
-                        width="10"
-                        height="10"
-                        viewBox="0 0 8 8"
-                        fill="currentColor"
-                        className="absolute -right-1.5 -top-1.5 origin-center animate-spin-every-once-in-a-while text-black dark:text-white"
-                        xmlns="http://www.w3.org/2000/svg"
+                    <DropdownMenu.Content
+                        className="flex flex-col border-2 border-black bg-white dark:border-white dark:bg-black"
+                        sideOffset={4}
+                        align="center"
+                        side="top"
                     >
-                        <path
-                            d="M6.45795 2.71729L1.03912 5.25967"
-                            stroke="black"
-                            stroke-width="1.5"
-                        />
-                        <path
-                            d="M2.48865 1.3031L5.00845 6.67379"
-                            stroke="black"
-                            stroke-width="1.5"
-                        />
-                    </svg>
-                )}
-            </Link>
-            <div className="hidden md:block">/</div>
-            <EmailLink
-                className={'svg-outline hidden shrink-0 font-normal md:block'}
-            >
-                email me feedback
-            </EmailLink>
-            <div>/</div>
-            <Link href={'https://discord.com/invite/a9eYv4sgWp '}>discord</Link>
+                        <DropdownMenu.Item asChild={true}>
+                            <Link
+                                className="cursor-pointer px-2 py-1 no-underline outline-none focus:bg-black focus:text-white dark:focus:bg-white dark:focus:text-black"
+                                href={'/copyright'}
+                            >
+                                copyright
+                            </Link>
+                        </DropdownMenu.Item>
+                        <DropdownMenu.Item asChild={true}>
+                            <Link
+                                className="cursor-pointer px-2 py-1 no-underline outline-none focus:bg-black focus:text-white dark:focus:bg-white dark:focus:text-black"
+                                href={'/privacy-policy'}
+                            >
+                                privacy
+                            </Link>
+                        </DropdownMenu.Item>
+                        <DropdownMenu.Item asChild={true}>
+                            <Link
+                                className="cursor-pointer px-2 py-1 no-underline outline-none focus:bg-black focus:text-white dark:focus:bg-white dark:focus:text-black"
+                                href={'/terms-of-service'}
+                            >
+                                terms
+                            </Link>
+                        </DropdownMenu.Item>
+                    </DropdownMenu.Content>
+                </DropdownMenu.Root>
+
+                <div>/</div>
+                <Link
+                    className="svg-outline relative no-underline"
+                    href={'/why'}
+                >
+                    why?
+                </Link>
+            </div>
+            <div className="hidden flex-grow md:block"> </div>
+            {/* <div className="block md:hidden">/</div> */}
+            <div className="flex items-center justify-between space-x-3">
+                <Link
+                    className="svg-outline relative no-underline"
+                    href={'https://github.com/joshuawootonn/type-the-word'}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    github
+                </Link>
+
+                <div>/</div>
+                <Link
+                    className={clsx(`svg-outline relative`)}
+                    href={'/changelog'}
+                >
+                    changelog
+                    {hasUnreadChangelog && (
+                        <svg
+                            width="10"
+                            height="10"
+                            viewBox="0 0 8 8"
+                            fill="currentColor"
+                            className="absolute -right-1.5 -top-1.5 origin-center animate-spin-every-once-in-a-while text-black dark:text-white"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                d="M6.45795 2.71729L1.03912 5.25967"
+                                stroke="black"
+                                stroke-width="1.5"
+                            />
+                            <path
+                                d="M2.48865 1.3031L5.00845 6.67379"
+                                stroke="black"
+                                stroke-width="1.5"
+                            />
+                        </svg>
+                    )}
+                </Link>
+                <div className="hidden sm:block">/</div>
+                <EmailLink
+                    className={
+                        'svg-outline hidden shrink-0 font-normal sm:block'
+                    }
+                >
+                    email me feedback
+                </EmailLink>
+                <div>/</div>
+                <Link href={'https://discord.com/invite/a9eYv4sgWp '}>
+                    discord
+                </Link>
+            </div>
         </footer>
     )
 }
