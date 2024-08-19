5555a709eac0fc5a2ba75abdbbce9c81b297aba9
Joshua Wootonn <joshuawootonn@gmail.com>
2024-08-19 07:48:50 -0400

    fix: remove extra padding from last commit

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -26,7 +26,7 @@ export function Footer() {
         !hasSeenChangelog || (hasSeenChangelog && !hasSeenEveryChangelog)
 
     return (
-        <footer className="m-8 flex w-full items-center justify-between space-x-3 py-2 text-sm dark:text-white">
+        <footer className="flex w-full items-center justify-between space-x-3 py-2 text-sm dark:text-white">
             <a
                 className="svg-outline relative no-underline"
                 href="https://www.esv.org/"
