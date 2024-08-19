760c0dc34eee890d930168c9037fde71b7d4cd9f
Joshua Wootonn <joshuawootonn@gmail.com>
2024-08-19 08:23:26 -0400

    fix: fix mobile overview section not showing boxes

--- src/app/history/history-overview.tsx
+++ src/app/history/history-overview.tsx
@@ -45,7 +45,7 @@ export function HistoryOverview({ overview }: { overview: BookOverview[] }) {
                                                         xmlns="http://www.w3.org/2000/svg"
                                                         viewBox="0 0 16 16"
                                                         className={clsx(
-                                                            'h-full w-full border-2 border-black text-white dark:border-white dark:text-black',
+                                                            'h-full w-full border-2 border-black dark:border-white',
                                                         )}
                                                     >
                                                         <rect
@@ -57,8 +57,7 @@ export function HistoryOverview({ overview }: { overview: BookOverview[] }) {
                                                                           18) /
                                                                       100
                                                             }
-                                                            fill="currentColor"
-                                                            className="invert"
+                                                            className="fill-black dark:fill-white"
                                                             x="-1"
                                                             y="-1"
                                                         />
@@ -67,9 +66,7 @@ export function HistoryOverview({ overview }: { overview: BookOverview[] }) {
                                                                 fillRule="evenodd"
                                                                 d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
                                                                 clipRule="evenodd"
-                                                                fill={
-                                                                    'currentColor'
-                                                                }
+                                                                className="fill-white dark:fill-black"
                                                             />
                                                         )}
                                                     </svg>
