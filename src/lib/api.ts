9ca92c97346b31c35d098bf8263a395183a0967f
Joshua Wootonn <joshuawootonn@gmail.com>
2024-12-23 06:00:18 -0600

    chore: remove the last instance of port 3000

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -25,7 +25,7 @@ export function getBaseUrl() {
 
     if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`
 
-    return `http://localhost:${process.env.PORT ?? 3000}`
+    return `http://localhost:${process.env.PORT ?? 3010}`
 }
 
 export async function fetchPassage(
