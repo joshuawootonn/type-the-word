58b2e77052c9d4342b7449ee7916eb6bd94ed070
Joshua Wootonn <joshuawootonn@gmail.com>
2025-02-02 13:59:30 -0500

    feat: create box component

--- src/app/history/history-overview.tsx
+++ src/app/history/history-overview.tsx
@@ -5,6 +5,43 @@ import clsx from 'clsx'
 import Link from 'next/link'
 import { BookOverview } from './overview'
 
+function Box({ percentage }: { percentage: number }) {
+    const isComplete = percentage >= 100
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            className={clsx('h-full w-full')}
+        >
+            <rect
+                height="14"
+                width="14"
+                strokeWidth="1"
+                className="stroke-primary "
+                fill="none"
+                x="1"
+                y="1"
+            />
+            <rect
+                height="14"
+                width={isComplete ? 14 : (percentage * 14) / 100}
+                strokeWidth="1"
+                className="fill-primary stroke-primary"
+                x="1"
+                y="1"
+            />
+            {isComplete && (
+                <path
+                    fillRule="evenodd"
+                    d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
+                    clipRule="evenodd"
+                    className="fill-secondary"
+                />
+            )}
+        </svg>
+    )
+}
+
 export function HistoryOverview({ overview }: { overview: BookOverview[] }) {
     return (
         <Tooltip.Provider delayDuration={0}>
@@ -30,82 +67,39 @@ export function HistoryOverview({ overview }: { overview: BookOverview[] }) {
                                 </div>
                             </summary>
                             <div className="grid grid-cols-[repeat(auto-fill,_minmax(max(36px),_1fr))] py-4">
-                                {book.chapters.map((chapterOverview, j) => {
-                                    const isComplete =
-                                        chapterOverview.percentage >= 100
-                                    return (
-                                        <Tooltip.Root key={j}>
-                                            <Tooltip.Trigger asChild>
-                                                <Link
-                                                    href={`/passage/${book.book}_${chapterOverview.chapter}`}
-                                                    prefetch={false}
-                                                    className="svg-outline-xs relative aspect-square h-full p-1 outline-none"
-                                                >
-                                                    <svg
-                                                        xmlns="http://www.w3.org/2000/svg"
-                                                        viewBox="0 0 16 16"
-                                                        className={clsx(
-                                                            'h-full w-full',
-                                                        )}
-                                                    >
-                                                        <rect
-                                                            height="14"
-                                                            width="14"
-                                                            strokeWidth="1"
-                                                            className="stroke-primary "
-                                                            fill="none"
-                                                            x="1"
-                                                            y="1"
-                                                        />
-                                                        <rect
-                                                            height="14"
-                                                            width={
-                                                                isComplete
-                                                                    ? 14
-                                                                    : (chapterOverview.percentage *
-                                                                          14) /
-                                                                      100
-                                                            }
-                                                            strokeWidth="1"
-                                                            className="fill-primary stroke-primary"
-                                                            x="1"
-                                                            y="1"
-                                                        />
-                                                        {isComplete && (
-                                                            <path
-                                                                fillRule="evenodd"
-                                                                d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
-                                                                clipRule="evenodd"
-                                                                className="fill-secondary"
-                                                            />
-                                                        )}
-                                                    </svg>
-                                                </Link>
-                                            </Tooltip.Trigger>
-                                            <Tooltip.Content
-                                                className="prose grid select-none grid-cols-[1fr_minmax(30px,min-content)] gap-x-3 gap-y-1 border-2 border-primary bg-secondary px-3 py-2 font-sans leading-none text-primary "
-                                                sideOffset={2}
+                                {book.chapters.map((chapterOverview, j) => (
+                                    <Tooltip.Root key={j}>
+                                        <Tooltip.Trigger asChild>
+                                            <Link
+                                                href={`/passage/${book.book}_${chapterOverview.chapter}`}
+                                                prefetch={false}
+                                                className="svg-outline-xs relative aspect-square h-full p-1 outline-none"
                                             >
-                                                <div>Chapter: </div>
-                                                <div>
-                                                    {chapterOverview.chapter}
-                                                </div>
-                                                <div>Verses: </div>
-                                                <div>
-                                                    {
-                                                        chapterOverview.typedVerses
+                                                <Box
+                                                    percentage={
+                                                        chapterOverview.percentage
                                                     }
-                                                    /{chapterOverview.verses}
-                                                </div>
-                                                <div>Completion: </div>
-                                                <div>
-                                                    {chapterOverview.percentage}
-                                                    %
-                                                </div>
-                                            </Tooltip.Content>
-                                        </Tooltip.Root>
-                                    )
-                                })}
+                                                />
+                                            </Link>
+                                        </Tooltip.Trigger>
+                                        <Tooltip.Content
+                                            className="prose grid select-none grid-cols-[1fr_minmax(30px,min-content)] gap-x-3 gap-y-1 border-2 border-primary bg-secondary px-3 py-2 font-sans leading-none text-primary "
+                                            sideOffset={2}
+                                        >
+                                            <div>Chapter: </div>
+                                            <div>{chapterOverview.chapter}</div>
+                                            <div>Verses: </div>
+                                            <div>
+                                                {chapterOverview.typedVerses}/
+                                                {chapterOverview.verses}
+                                            </div>
+                                            <div>Completion: </div>
+                                            <div>
+                                                {chapterOverview.percentage}%
+                                            </div>
+                                        </Tooltip.Content>
+                                    </Tooltip.Root>
+                                ))}
                             </div>
                         </details>
                     )
