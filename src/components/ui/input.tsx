51ac495af842529fb934a32f63baf5db69d16496
Joshua Wootonn <joshuawootonn@gmail.com>
2026-01-19 11:12:06 -0600

    feat: create input primitive

---
+++ src/components/ui/input.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import clsx from "clsx"
+import { forwardRef, ComponentPropsWithoutRef, ElementRef } from "react"
+
+const Input = forwardRef<
+    ElementRef<"input">,
+    ComponentPropsWithoutRef<"input">
+>(({ className, ...props }, ref) => (
+    <div className="svg-outline group relative">
+        <div className="svg-outline-override absolute -z-10 hidden group-focus-within:block" />
+        <input
+            ref={ref}
+            className={clsx(
+                "w-full border-2 border-primary bg-secondary px-3 py-2 outline-none",
+                className,
+            )}
+            {...props}
+        />
+    </div>
+))
+Input.displayName = "Input"
+
+const Textarea = forwardRef<
+    ElementRef<"textarea">,
+    ComponentPropsWithoutRef<"textarea">
+>(({ className, ...props }, ref) => (
+    <div className="svg-outline group relative">
+        <div className="svg-outline-override absolute -z-10 hidden group-focus-within:block" />
+        <textarea
+            ref={ref}
+            className={clsx(
+                "w-full border-2 border-primary bg-secondary px-3 py-2 outline-none",
+                className,
+            )}
+            {...props}
+        />
+    </div>
+))
+Textarea.displayName = "Textarea"
+
+export { Input, Textarea }
