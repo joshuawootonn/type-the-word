588e0ae0d1b762ced59590661c97a38066bc8f44
Joshua Wootonn <joshuawootonn@gmail.com>
2026-02-14 20:15:17 -0600

    fix: fix the icon and animation that is being used on docs heading

--- src/components/docs/mdx-components.tsx
+++ src/components/docs/mdx-components.tsx
@@ -1,42 +1,10 @@
 import type { MDXComponents } from "mdx/types"
 import type { ComponentPropsWithoutRef, ReactElement } from "react"
 
+import { LinkSimple } from "@phosphor-icons/react/dist/ssr"
+
 type HeadingTwoProps = ComponentPropsWithoutRef<"h2">
 type HeadingThreeProps = ComponentPropsWithoutRef<"h3">
-
-function HeadingAnchorIcon(): ReactElement {
-    return (
-        <svg
-            aria-hidden
-            viewBox="0 0 256 256"
-            className="h-4 w-4"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-        >
-            <path
-                d="M117.6 174.4L81.6 210.4C63.9 228.1 35.1 228.1 17.4 210.4C-0.3 192.7 -0.3 163.9 17.4 146.2L53.4 110.2"
-                stroke="currentColor"
-                strokeWidth="16"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-            />
-            <path
-                d="M138.4 81.6L174.4 45.6C192.1 27.9 220.9 27.9 238.6 45.6C256.3 63.3 256.3 92.1 238.6 109.8L202.6 145.8"
-                stroke="currentColor"
-                strokeWidth="16"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-            />
-            <path
-                d="M92 164L164 92"
-                stroke="currentColor"
-                strokeWidth="16"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-            />
-        </svg>
-    )
-}
 
 function DocsHeadingTwo({ id, ...props }: HeadingTwoProps): ReactElement {
     const { className, children, ...rest } = props
@@ -56,8 +24,8 @@ function DocsHeadingTwo({ id, ...props }: HeadingTwoProps): ReactElement {
                     href={`#${id}`}
                     className="group/link relative -ml-6 inline-block pl-6 no-underline"
                 >
-                    <span className="pointer-events-none absolute top-1/2 left-0 -translate-y-1/2 opacity-0 transition-opacity group-hover/link:opacity-100 group-focus-visible/link:opacity-100">
-                        <HeadingAnchorIcon />
+                    <span className="pointer-events-none absolute top-1/2 left-0 -translate-y-1/2 opacity-0 group-hover/link:opacity-100 group-focus-visible/link:opacity-100">
+                        <LinkSimple aria-hidden size={16} weight="regular" />
                     </span>
                     {children}
                 </a>
@@ -84,8 +52,8 @@ function DocsHeadingThree({ id, ...props }: HeadingThreeProps): ReactElement {
                     href={`#${id}`}
                     className="group/link relative -ml-6 inline-block pl-6 no-underline"
                 >
-                    <span className="pointer-events-none absolute top-1/2 left-0 -translate-y-1/2 opacity-0 transition-opacity group-hover/link:opacity-100 group-focus-visible/link:opacity-100">
-                        <HeadingAnchorIcon />
+                    <span className="pointer-events-none absolute top-1/2 left-0 -translate-y-1/2 opacity-0 group-hover/link:opacity-100 group-focus-visible/link:opacity-100">
+                        <LinkSimple aria-hidden size={16} weight="regular" />
                     </span>
                     {children}
                 </a>
